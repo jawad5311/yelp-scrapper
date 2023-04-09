c91ee48ba9af152b6894038969232caf5e3519d6
Jawad <muhammadjawad5311@gmail.com>
2023-04-09 16:14:19 +0500

    pagination functionality added

--- src/yelp_scrapy_spider/yelp_scrapy_spider/settings.py
+++ src/yelp_scrapy_spider/yelp_scrapy_spider/settings.py
@@ -77,9 +77,9 @@ ROBOTSTXT_OBEY = False
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
 AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-AUTOTHROTTLE_START_DELAY = 5
+AUTOTHROTTLE_START_DELAY = 2
 # The maximum download delay to be set in case of high latencies
-AUTOTHROTTLE_MAX_DELAY = 60
+AUTOTHROTTLE_MAX_DELAY = 20
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
 #AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
--- src/yelp_scrapy_spider/yelp_scrapy_spider/spiders/yelp.py
+++ src/yelp_scrapy_spider/yelp_scrapy_spider/spiders/yelp.py
@@ -8,7 +8,8 @@ class YelpSpider(scrapy.Spider):
 
     def parse(self, response):
         urls = response.css('h3.css-1agk4wl span a.css-1m051bw::attr(href)').getall()
-
+        next_page_url = response.css('.next-link::attr(href)').get()
+        print(f'THIS IS NEXT PAGE: {next_page_url}')
         for url in urls:
             url = f'http://www.yelp.com{url}'
             yield scrapy.Request(
@@ -16,9 +17,19 @@ class YelpSpider(scrapy.Spider):
                 callback=self.parse_item
             )
 
+        if next_page_url:
+            yield scrapy.Request(
+                url=next_page_url,
+                callback=self.parse
+            )
+
     def parse_item(self, response):
         name = response.css("h1.css-1se8maq::text").get()
         phone_no = response.css(".border-color--default__09f24__NPAKY+ .border--top__09f24__exYYb .vertical-align-middle__09f24__zU9sE .css-1p9ibgf::text").get()
+        try:
+            phone_no = phone_no.replace('(', '').replace(')', '').replace('-', '').replace(' ', '').strip()
+        except AttributeError:
+            pass
         website = response.css(".css-1p9ibgf .css-1um3nx::text").get()
 
         # Business Address
